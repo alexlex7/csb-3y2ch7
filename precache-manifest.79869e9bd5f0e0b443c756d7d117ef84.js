6388a68b0be7110a7282ee34991c31b7cfadf2a3
Code Sandbox <hello@codesandbox.io>
2022-06-10 07:05:09 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.79869e9bd5f0e0b443c756d7d117ef84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08af42f169e0cb28b154eae1c24a1872",
+    "url": "https://alexlex7.github.io/csb-3y2ch7/index.html"
+  },
+  {
+    "revision": "e49a5c5a73a41bd72848",
+    "url": "https://alexlex7.github.io/csb-3y2ch7/static/css/main.184c6a5a.chunk.css"
+  },
+  {
+    "revision": "fc8aa995bc0f880eb117",
+    "url": "https://alexlex7.github.io/csb-3y2ch7/static/js/2.05e68a68.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "https://alexlex7.github.io/csb-3y2ch7/static/js/2.05e68a68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e49a5c5a73a41bd72848",
+    "url": "https://alexlex7.github.io/csb-3y2ch7/static/js/main.1f3f3e17.chunk.js"
+  },
+  {
+    "revision": "718c8ee84552dc4b922b",
+    "url": "https://alexlex7.github.io/csb-3y2ch7/static/js/runtime-main.a2c6fdeb.js"
+  }
+]);
